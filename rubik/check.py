d34fd9bf2b50b3c069fb6e49df6e173b6240ef8c
BarrettPurohitY <barrettpurohit@outlook.com>
2022-01-28 20:53:14 -0600

    First Update

--- rubik/check.py
+++ rubik/check.py
@@ -2,9 +2,16 @@ import rubik.cube as rubik
 
 def _check(parms):
     result={}
-    encodedCube = parms.get('cube',None)       
+    encodedCube = parms.get('cube', None)       
     if(encodedCube == None):
-        result['status'] = 'error: xxx'
+        result['status'] = 'error: Cube Empty'
+    elif(type(encodedCube) != 'str'):
+        result['status'] = 'error: Incorrect Type'
+    elif(len(encodedCube) != 54):
+        result['status'] = 'error: Incorrect Number of Elements'
     else:
+        colorAmounts = {}
+        
+        
         result['status'] = 'ok'
     return result
--- rubik/test/checkTest.py
+++ rubik/test/checkTest.py
@@ -11,3 +11,35 @@ class CheckTest(TestCase):
         status = result.get('status', None)
         self.assertEqual(status, 'ok')
 
+    def test_check_020_ReturnErrorOnNull(self):
+        parm = {'op':'check',
+                'cube': None}
+        result = check._check(parm)
+        self.assertIn('status', result)
+        status = result.get('status', None)
+        self.assertEqual(status, 'error: Cube Empty')
+
+    def test_check_030_ReturnErrorOnIncorrectType(self):
+        parm = {'op':'check',
+                'cube': 42}
+        result = check._check(parm)
+        self.assertIn('status', result)
+        status = result.get('status', None)
+        self.assertEqual(status, 'error: Incorrect Type')
+        
+    def test_check_040_ReturnErrorOnIncorrectNumberOfElements(self):
+        parm = {'op':'check',
+                'cube':'bbbbbbbbbrrrrrrrrrgggggggggoooooooooyyyyyyyyy'}
+        result = check._check(parm)
+        self.assertIn('status', result)
+        status = result.get('status', None)
+        self.assertEqual(status, 'error: Incorrect Number of Elements')
+   
+    def test_check_050_ReturnErrorOnUnequalColors(self):
+        parm = {'op':'check',
+                'cube':'bbbbbbbbbrrrrrrrrrgggggggggoooooooooyyyyyyyyyyyyyyyyyy'}
+        result = check._check(parm)
+        self.assertIn('status', result)
+        status = result.get('status', None)
+        self.assertEqual(status, 'error: Need 9 Occurrences of 6 Colors')
+          
